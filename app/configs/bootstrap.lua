4a29b17c7a2c5543addeb1f3f891b105e2ec9b52
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-06-14 17:02:56 -0500

    sobreescribiendo 404

--- app/configs/bootstrap.lua
+++ app/configs/bootstrap.lua
@@ -3,7 +3,17 @@ local home_routes = require("app.routes.home")
 local bootstrap = {}
 
 function bootstrap.load(app)
+  -- Montar las rutas bajo "/"
   app:include(home_routes)
+  -- Definir el manejador de 404
+  function app:handle_404(self)
+    return {
+      status = 404,
+      render = "not_found"
+    }
+  end
 end
+
+
 
 return bootstrap
